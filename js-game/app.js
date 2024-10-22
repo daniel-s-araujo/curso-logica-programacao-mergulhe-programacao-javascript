168e1bbc91ebffb40c82d7585f27469675e09455
Daniel Araújo <daniels_araujo@outlook.com.br>
2024-10-22 13:42:48 -0300

    Novas condições

--- js-game/app.js
+++ js-game/app.js
@@ -1,10 +1,14 @@
 alert("Boas vindas ao jogo no número secreto");
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
 let chute = prompt("Escolha um número entre 1 e 10");
 
 if (chute == numeroSecreto) {
   alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-  alert("Você errou :(");
+  if (chute > numeroSecreto) {
+    alert(`O número secreto é menor que ${chute}`);
+  } else {
+    alert(`O número secreto é maior que ${chute}`);
+  }
 }
